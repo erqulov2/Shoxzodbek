41a8ca41d096b374195c4c81edda766de74594cd
Shoxzodbek <Erqulov Shoxzodbek>
2024-11-29 20:38:07 +0500

    Initial commit

---
+++ array.go
@@ -0,0 +1,124 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+	//var numbers [5]int
+
+	//fmt.Println(numbers)
+
+	//numbers =[5]int{1, 2, 3, 4, 5}
+
+	//fmt.Println(numbers)
+
+	//fmt.Println(numbers[0])
+
+	//numbers[3] = 8
+	//fmt.Println(numbers)
+
+	//var arr = make([]int, 6)
+	//fmt.Println(arr)
+
+	//for i := 0; i < 10; i++ {
+	//	arr = append(arr, i)
+	//}
+	//fmt.Println(arr)
+
+	//UYga vazifa 1
+
+	// numbers := [5]int{1, 2, 3, 4, 5}
+
+	// sum := 0
+	// for i := 0; i < len(numbers); i++ {
+	// sum += numbers[i]
+	// }
+
+	// fmt.Println("Array elementlari summasi:", sum)
+
+	//Uyga vazifa 2
+
+	// num := []int{4, 6, 9, 1, 8, 2, 77}
+	// max := num[0]
+	// for i := 1; i < len(num); i++{
+
+	// 	if num[i] > max {
+	// 		max = num[i]
+	// }
+	// }
+	// fmt.Println("Eng katta qiymat:", max)
+
+	//Uyga vazifa 3
+
+	// var num [5]int
+	// num[0] = -9
+	// num[1] = 4
+	// num[2] = 2
+	// num[3] = 7
+	// num[4] = 3
+
+	// oddCount := 0
+	// evenCount := 0
+
+	// for _, n := range num {
+	// 	if n%2 == 0 {
+	// 		evenCount++
+	// 	} else {
+	// 		oddCount++
+	// 	}
+	// }
+	// fmt.Println("juft sunlar:", evenCount)
+	// fmt.Println("toq sonlar:", oddCount)
+
+	//Uyga vazifa 4
+	
+	// type Pupil struct {
+	// 	Age int
+	// 	Class string
+	// 	Grade int
+	// }
+
+	// var pupil Pupil
+
+	// pupil.Age = 15
+	// pupil.Class = "to'qqiz a"
+	// pupil.Grade = 4
+	// fmt.Println(pupil)
+    
+
+
+	//m := make(map[string]Pupil)
+
+	// m = map[string]Pupil {
+	// 	"Sanjar": Pupil{
+	// 		Age: 14,
+	// 		Grade: 3,
+	// 	},
+	// }
+	// fmt.Println(m["Sanjar"])
+	// var name string
+	// fmt.Println("Ism kiriting:")
+	// fmt.Scanln(&name)
+
+	// fmt.Println("Yoshini kiriting:")
+	// fmt.Scanln(&pupil.Age)
+
+	// fmt.Println("Sinfini kiriting:")
+	// fmt.Scanln(&pupil.Class)
+
+	// fmt.Println("Baxosini kiriting:")
+	// fmt.Scanln(&pupil.Grade)
+
+	// 	m[name] = pupil
+	// 	fmt.Println(m)
+    
+    // git - version control sitem
+	 
+	//}
+
+	package main
+
+	import "fmt"
+
+	func main() {
+		fmt.Println("Hello World!")
+	}
